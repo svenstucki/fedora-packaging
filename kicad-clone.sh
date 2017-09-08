c8b9127fb90556962dd5827599af11829b935740
Sven Stucki <git@svenstucki.ch>
2017-09-08 12:16:26 +0200

    Shallow clone repos to save space and traffic

--- kicad-clone.sh
+++ kicad-clone.sh
@@ -2,21 +2,21 @@
 set -e
 set -x
 if [ -d kicad.bzr ]; then
-	cd kicad.bzr
-	git fetch origin
-	git reset --hard origin/master
-	cd ..
-else 
-	git clone https://git.launchpad.net/kicad kicad.bzr
+    cd kicad.bzr
+    git fetch origin
+    git reset --hard origin/master
+    cd ..
+else
+    git clone --depth 1 https://git.launchpad.net/kicad kicad.bzr
 fi
 
 if [ -d kicad-library.bzr ]; then
-	cd kicad-library.bzr
-	git fetch origin
-	git reset --hard origin/master
-	cd ..
+    cd kicad-library.bzr
+    git fetch origin
+    git reset --hard origin/master
+    cd ..
 else
-	git clone https://github.com/KiCad/kicad-library.git kicad-library.bzr
+    git clone --depth 1 https://github.com/KiCad/kicad-library.git kicad-library.bzr
 fi
 
 if [ -d kicad-i18n ]; then
@@ -25,16 +25,16 @@ if [ -d kicad-i18n ]; then
     git reset --hard origin/master
     cd ..
 else
-    git clone https://github.com/KiCad/kicad-i18n.git
+    git clone --depth 1 https://github.com/KiCad/kicad-i18n.git
 fi
 
 #TODO(mangelajo): pull the new doc builds
 #if [ -d kicad-doc.bzr ]; then
-#	cd kicad-doc.bzr
-#	bzr update 
-#	cd ..
+#    cd kicad-doc.bzr
+#    bzr update
+#    cd ..
 #else
-#	bzr branch --stacked lp:~kicad-developers/kicad/doc kicad-doc.bzr
+#    bzr branch --stacked lp:~kicad-developers/kicad/doc kicad-doc.bzr
 #fi
 
 
@@ -44,9 +44,7 @@ sed -n 's|.*\${KIGITHUB}/\([^)]*\)).*|\1|p'  kicad-library.bzr/template/fp-lib-t
 
 mkdir -p footprints
 cat footprint.list |while read FP
-	do
-		git clone https://github.com/KiCad/$FP footprints/$FP ||
-			print "$FP missing, possibly gone from GitHub now"
-	done
-
-
+    do
+        git clone https://github.com/KiCad/$FP footprints/$FP ||
+            print "$FP missing, possibly gone from GitHub now"
+    done
